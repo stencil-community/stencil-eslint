ec27fd812854e0493b8d8aae8a834511e51f464c
d0whc3r <d0whc3r@gmail.com>
2019-09-14 00:44:24 +0200

    fix: add all rules

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,23 +1,29 @@
+import asyncMethods from './async-methods';
+import banPrefix from './ban-prefix';
+import decoratorsContext from './decorators-context';
+import decoratorsStyle from './decorators-style';
+import hostDataDeprecated from './host-data-deprecated';
+import methodsMustBePublic from './methods-must-be-public';
+import preferVdomListener from './prefer-vdom-listener';
+import propsMustBePublic from './props-must-be-public';
+import propsMustBeReadonly from './props-must-be-readonly';
+import renderReturnsHost from './render-returns-host';
+import requiredPrefix from './required-prefix';
 import reservedMemberNames from './reserved-member-names';
 import singleExport from './single-export';
-import asyncMethods from './async-methods';
-import propsMustBePublic from './props-must-be-public';
-import methodsMustBePublic from './methods-must-be-public';
-import decoratorsContext from './decorators-context';
-import hostDataDeprecated from './host-data-deprecated';
-import preferVdomListener from './prefer-vdom-listener';
-import renderReturnsHost from './render-returns-host';
-import propsMustBeReadonly from './props-must-be-readonly';
 
 export default {
-  'reserved-member-names': reservedMemberNames,
-  'single-export': singleExport,
   'async-methods': asyncMethods,
-  'props-must-be-public': propsMustBePublic,
-  'methods-must-be-public': methodsMustBePublic,
+  'ban-prefix': banPrefix,
   'decorators-context': decoratorsContext,
+  'decorators-style': decoratorsStyle,
   'host-data-deprecated': hostDataDeprecated,
+  'methods-must-be-public': methodsMustBePublic,
   'prefer-vdom-listener': preferVdomListener,
+  'props-must-be-public': propsMustBePublic,
+  'props-must-be-readonly': propsMustBeReadonly,
   'render-returns-host': renderReturnsHost,
-  'props-must-be-readonly': propsMustBeReadonly
+  'required-prefix': requiredPrefix,
+  'reserved-member-names': reservedMemberNames,
+  'single-export': singleExport
 };
