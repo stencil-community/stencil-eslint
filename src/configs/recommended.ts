4a3bf27344a84c3bd8f179ff492838325823c6ec
d0whc3r <d0whc3r@gmail.com>
2019-09-15 00:48:15 +0200

    fix: update recommended rules with all rules

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -1,28 +1,40 @@
 export default {
-  overrides: [{
-    parser: "@typescript-eslint/parser",
-    files: ['*.ts', '*.tsx'],
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: "module",
-      ecmaFeatures: {
-        "jsx": true
-      }
-    },
-    rules: {
-      "@stencil/reserved-member-names": "error",
-      "@stencil/single-export": "error",
-      "@stencil/async-methods": "error",
-      "@stencil/props-must-be-public": "error",
-      "@stencil/methods-must-be-public": "error",
-      "@stencil/decorators-context": "error",
-      "@stencil/host-data-deprecated": "error",
-      "@stencil/prefer-vdom-listener": "error",
-      "@stencil/render-returns-host": "error",
-      "@stencil/props-must-be-readonly": "error"
-    },
-    plugins: [
-      "@stencil"
-    ]
-  }]
+  overrides: [
+    {
+      parser: '@typescript-eslint/parser',
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          'jsx': true
+        }
+      },
+      rules: {
+        '@stencil/async-methods': 'error',
+        '@stencil/ban-prefix': 'error',
+        '@stencil/decorators-context': ['error', ['stencil', 'st', 'stnl']],
+        '@stencil/decorators-style': [
+          'error', {
+            prop: 'inline',
+            state: 'inline',
+            element: 'inline',
+            event: 'inline',
+            method: 'multiline',
+            watch: 'multiline',
+            listen: 'multiline'
+          }],
+        '@stencil/host-data-deprecated': 'error',
+        '@stencil/methods-must-be-public': 'error',
+        '@stencil/prefer-vdom-listener': 'error',
+        '@stencil/props-must-be-public': 'error',
+        '@stencil/props-must-be-readonly': 'error',
+        '@stencil/render-returns-host': 'error',
+        '@stencil/reserved-member-names': 'error',
+        '@stencil/single-export': 'error'
+      },
+      plugins: [
+        '@stencil'
+      ]
+    }]
 };
