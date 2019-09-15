f59138bd5f0d0f18f80758994aeff3feade1b71f
d0whc3r <d0whc3r@gmail.com>
2019-09-15 02:01:10 +0200

    feat(required-jsdoc): new rule

---
+++ docs/required-jsdoc.md
@@ -0,0 +1,13 @@
+# required-jsdoc
+
+Ensures that all `Props`, `Methods` and `Events` have a valid jsdoc
+
+## Config
+
+No config is needed
+
+## Usage
+
+```json
+{ "@d0whc3r/stencil/required-jsdoc": "error" }
+```
---
+++ src/rules/required-jsdoc.ts
@@ -0,0 +1,60 @@
+import { Rule } from 'eslint';
+import { getDecorator, stencilComponentContext } from '../utils';
+
+const DECORATORS = ['Prop', 'Method', 'Event'];
+const INVALID_TAGS = ['type', 'memberof'];
+
+const rule: Rule.RuleModule = {
+  meta: {
+    docs: {
+      description: 'This rule catches Stencil Props and Methods using jsdoc.',
+      category: 'Possible Errors',
+      recommended: true
+    },
+    schema: []
+  },
+
+  create(context): Rule.RuleListener {
+    const stencil = stencilComponentContext();
+
+    const parserServices = context.parserServices;
+
+    function getJSDoc() {
+      return (node: any) => {
+        if (!stencil.isComponent()) {
+          return;
+        }
+
+        DECORATORS.forEach((decName) => {
+          if (getDecorator(node, decName)) {
+            const originalNode = parserServices.esTreeNodeToTSNodeMap.get(node);
+            const jsDoc = originalNode.jsDoc;
+            const isValid = jsDoc && jsDoc.length;
+            const haveTags = isValid &&
+                jsDoc.some((jsdoc: any) => jsdoc.tags && jsdoc.tags.length && jsdoc.tags.some(
+                    (tag: any) => INVALID_TAGS.includes(tag.tagName.escapedText.toLowerCase())));
+            if (!isValid) {
+              context.report({
+                node: node,
+                message: `The @${decName} decorator must to be documented.`
+              });
+            } else if (haveTags) {
+              context.report({
+                node: node,
+                message: `The @${decName} decorator have not valid tags (${INVALID_TAGS.join(', ')}).`
+              });
+            }
+          }
+        });
+      };
+    }
+
+    return {
+      ...stencil.rules,
+      'ClassProperty': getJSDoc(),
+      'MethodDefinition': getJSDoc()
+    };
+  }
+};
+
+export default rule;
