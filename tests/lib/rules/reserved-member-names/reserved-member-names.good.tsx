483dd534ede8ef518359c77d59d909b86bde8d8b
JC Franco <jfranco@esri.com>
2024-03-04 01:13:14 -0800

    fix(reserved-member-names): improve reserved member check by using `HTMLElement` (#101)
    * fix(reserved-member-names): improve reserved member check by testing against HTMLElement
    
    * fix attribute/prop check
    
    * update good test case

--- tests/lib/rules/reserved-member-names/reserved-member-names.good.tsx
+++ tests/lib/rules/reserved-member-names/reserved-member-names.good.tsx
@@ -1,6 +1,12 @@
 @Component({ tag: 'sample-tag' })
 export class SampleTag {
+  constructor() {
+    console.log("🚀");
+  }
+
   @Prop() sampleProp?: string;
+
+  @Method() async sampleMethod(): Promise<void> {}
 
   render() {
     return (<div>test</div>);
--- tests/lib/rules/reserved-member-names/reserved-member-names.wrong.tsx
+++ tests/lib/rules/reserved-member-names/reserved-member-names.wrong.tsx
@@ -1,9 +1,58 @@
 @Component({ tag: 'sample-tag' })
 export class SampleTag {
-  @Prop() title?: string;
+  // this component tests members from HTMLElement (including inherited)
 
+  // Global attribute
+  @Prop() autocapitalize?: string;
+
+  // Node prop
+  @Prop() isConnected?: string;
+
+  // Element prop
+  @Prop() id?: string;
+
+  // HTMLElement prop
+  @Prop() outerText?: string
+
+  // Global attribute attribute (as component prop)
+  @Prop() tabindex?: string;
+
+  // HTMLElement method (as component prop)
+  @Prop() blur?: string;
+
+  // EventTarget method
+  @Method()
+  async dispatchEvent() {
+    return 'ouch';
+  }
+
+  // Node method
+  @Method()
+  async contains() {
+    return 'ouch';
+  }
+
+  // Element method
+  @Method()
+  async closest() {
+    return 'ouch';
+  }
+
+  // HTMLElement method
+  @Method()
+  async focus() {
+    return 'ouch';
+  }
+
+  // Element prop (as component method)
   @Method()
   async innerHTML() {
+    return 'ouch';
+  }
+
+  // Global attribute (as component method)
+  @Method()
+  async contextmenu() {
     return 'ouch';
   }
 
