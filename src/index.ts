2ac8bd02a8b580de00650fe1819708ddb67aae36
John Jenkins <john.jenkins@nanoporetech.com>
2025-02-03 13:02:14 +0000

    feat: add support for eslint flat config (eslint >= 9)

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,44 @@
  * @author Tom Chinery &lt;tom.chinery@addtoevent.co.uk&gt;
  */
 
+// @ts-expect-error - no types
+import react from 'eslint-plugin-react';
 import rules from './rules';
 import configs from './configs';
 
-export {
+const plugin = {
   rules,
   configs
 };
+
+const flatBase = {
+  plugins: { '@stencil-community': plugin },
+  rules: configs.base.overrides[0].rules,
+  languageOptions: { parserOptions: configs.base.overrides[0].parserOptions },
+}
+
+const flatRecommended = {
+  plugins: { 
+    react: react, 
+    '@stencil-community': plugin 
+  },
+  rules: configs.recommended.rules,
+  languageOptions: { parserOptions: configs.base.overrides[0].parserOptions },
+}
+
+const flatStrict = {
+  plugins: { 
+    react: react, 
+    '@stencil-community': plugin 
+  },
+  rules: configs.strict.rules,
+  languageOptions: { parserOptions: configs.base.overrides[0].parserOptions },
+}
+
+configs.flat = {
+  base: flatBase,
+  recommended: flatRecommended,
+  strict: flatStrict,
+}
+
+export default plugin;
